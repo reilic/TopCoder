74ca10af4d6f73af485d32cd56d4eb6c09985603
reilic <erik.li@gmail.com>
2016-08-26 15:55:12 +1000

    add __str__

--- oop/student.py
+++ oop/student.py
@@ -10,13 +10,17 @@ class Person(object):
 		self.name = n
 		print 'New Name is: %r' % self.name
 
+	def __repr__(self):
+		return "%15s%s\n" % ("Name: ", self.name)
+
 class Student(Person):
 
 	def __init__(self, n, sid, d):
 		Person.__init__(self,n)
 		self.sid = sid
 		self.degree = d
-
+#		print Student.__str__(self)
+		
 	def getStudentID(self):
 		return self.sid
 
@@ -26,14 +30,21 @@ class Student(Person):
 	def setDegree(self,d):
 		self.degree = d
 
-class PhDtudent(Student):
+	def __str__(self):
+		return Person.__str__(self) + "%15s%s\n%15s%s\n"  % ("StudentID: ", self.sid, "Degree: ", self.degree)
+
+class PhDStudent(Student):
 
 	def  __init__(self,n,sid,degree, thesis):
 		Student.__init__(self,n,sid,degree)
 		self.thesis = thesis
+#		print PhDStudent.__str__(self)
 
 	def getThesis(self):
 		return self.thesis
+
+	def __str__(self):
+		return Student.__str__(self) + "%15s%s\n" % ("Thesis: ", self.thesis)
 
 class MasterStudent(Student):
 
@@ -44,30 +55,19 @@ class MasterStudent(Student):
 	def getSupervisor(self):
 			return self.supervisor
 
-Cameron = Student('Cameron', 'U2514483', "Social Science")
-	
-print 'My Name is %s \nMy UnivID is %s' % (Cameron.getName(), Cameron.getStudentID())
+	def setDegree(self,degree, supervisor):
+		self.degree = degree
+		self.supervisor = supervisor
+
+	def __str__(self):
+		return Student.__str__(self) + "%15s%s\n" % ("Supervisor: ", self.supervisor)
 
 Jeff = Person("Jeff")
 
-print Jeff.getName()
+Cameron = Student('Cameron', 'U2514483', "Social Science")
+Simon = PhDStudent('Simon', 'U9012334', 'Computer Science', 'Impact of Compter on mice reproduction')
+#Ellan = MasterStudent("Ellan", 'U4354830', "Photography", "Adam Depands")
+#Ellan.setDegree("News Photography", "John Simonthian")
 
-Simon = PhDtudent('Simon', 'U9012334', 'Computer Science', 'Impact of Compter on mice reproduction')
-
-print Simon.getDegree()
-
-Simon.setDegree("Earth Science")
-
-print Simon.getDegree()
-
-print Simon.getName()
-
-print Simon.getThesis()
-
-Ellan = MasterStudent("Ellan", 'U4354830', "Photography", "Adam Depands")
-
-print Ellan.getDegree()
-
-print Ellan.getName()
-
-print Ellan.getSupervisor()
+print Cameron
+print Simon
